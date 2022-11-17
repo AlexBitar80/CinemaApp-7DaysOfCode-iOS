b9dad0a1bda9cd2451600dfad6fe124b90cd1ff2
joão Alexandre Bitar <joaoalexbitar@brq.com>
2022-11-17 00:00:02 -0300

    added static image of movie and release date

--- Cine-App/Model/Movie.swift
+++ Cine-App/Model/Movie.swift
@@ -15,6 +15,15 @@ struct Movie: Codable {
     let overview: String?
     let voteAverage: Double?
     
+    static var movies: [Movie] {
+        return [
+            Movie(id: 2, title: "Homem Aranha", releaseDate: "2020-05-04", image: "thor-image", overview: "", voteAverage: 5.0),
+            Movie(id: 4, title: "Órfã 2: A Origem", releaseDate: "2022-07-27", image: "thor-image", overview: "", voteAverage: 7.8),
+            Movie(id: 5, title: "Minions 2: A Origem de Gru", releaseDate: "2022-06-29", image: "thor-image", overview: "", voteAverage: 7.5),
+            Movie(id: 7, title: "Thor: Amor e Trovão", releaseDate: "2020-05-04", image: "thor-image", overview: "", voteAverage: 8.0)
+        ]
+    }
+    
     enum codingKeys: String, CodingKey {
         case id = "id"
         case title = "title"
